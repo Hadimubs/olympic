4167145dbc8eba0fab6b09ef20b69922f13e38fe
Hadimubs <123986326+Hadimubs@users.noreply.github.com>
2023-05-24 20:57:21 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+canvas=document.getElementById("myCanvas");
+ctx.beginPath();
+ctx.strokeStyle="red" ;
+ctx.lineWidth=3;
+ctx.rect(150,143,430,200);
+ctx.strokeStyle();
+
+
+ctx.beginpath();
+ctx.strokeStyle="blue";
+ctx.linewidth=5;
+ctx.arc(250,210,40,0,2*Math.PI)
+ctx.strokeStyle();
